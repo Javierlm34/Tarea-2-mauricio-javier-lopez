f33046ae503cf3e0c10df675b816bcc8cc91f0e7
Javierlm34 <lopjavi202@gmail.com>
2020-10-20 19:51:55 -0500

    Initial commit

---
+++ Assets/nombre.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nombre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mauricio Javier López Morales");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
